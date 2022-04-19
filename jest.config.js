a05792f1b357c3a9cc7209b61bcc03a1d65566fd
Italo Gabriel <italog03@gmail.com>
2022-04-19 20:55:58 -0300

    chore: change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'babel',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
