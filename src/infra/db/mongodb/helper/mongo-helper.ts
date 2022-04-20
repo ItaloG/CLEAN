1d5f4440eab291e7588385557b73951055b1e845
Italo Gabriel <italog03@gmail.com>
2022-04-20 15:48:37 -0300

    feat: ensure AccountMongoRepository returns an account on succes

--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -1,14 +1,25 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as any as MongoClient,
+  uri: null as any as string,
+
   async connect (uri: string): Promise<void> {
-    this.client = MongoClient.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    this.uri = uri
+    this.client = await MongoClient.connect(uri)
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   }
 }
