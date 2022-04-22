eebfa6374182e4bd23195150e250b410b1b17507
Italo Gabriel <italog03@gmail.com>
2022-04-22 10:43:35 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,15 +1,29 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
-        name: '',
-        email: '',
-        password: '',
-        passwordConfirmation: ''
+        name: 'Italo',
+        email: 'italo.pereira@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
       })
       .expect(200)
   })
