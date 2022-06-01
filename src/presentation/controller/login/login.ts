ed6c3f0c7fb92fe59635a8313ac17df20a26f637
Italo Gabriel <italog03@gmail.com>
2022-06-01 11:16:57 -0300

    refactor: get params from httpRequest

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -6,11 +6,13 @@ import { Controller, EmailValidator } from '../singup/sigup-protocols'
 export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+    const { email, password } = httpRequest.body
 
-    if (!httpRequest.body.password) return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+    if (!email) return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!password) return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+
+    const isValid = this.emailValidator.isValid(email)
     if (!isValid) return badRequest(new InvalidParamError('email'))
 
     return {
