e9e110293ce2143f50775308fb36dcd148984bff
Italo Gabriel <italog03@gmail.com>
2022-06-03 11:40:46 -0300

    refactor: rename TokenGenerator to Encrypter

--- src/data/usucases/authentication/db-authentication.ts
+++ src/data/usucases/authentication/db-authentication.ts
@@ -1,10 +1,10 @@
-import { Authentication, LoadAccountByEmailRepository, HashComparer, TokenGenerator, UpdateAccessTokenRepository, AuthenticationModel } from './db-authentication-protocols'
+import { Authentication, LoadAccountByEmailRepository, HashComparer, Encrypter, UpdateAccessTokenRepository, AuthenticationModel } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGenerator: TokenGenerator,
+    private readonly encrypter: Encrypter,
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
@@ -18,7 +18,7 @@ export class DbAuthentication implements Authentication {
         account.password
       )
       if (isValid) {
-        const accessToken = await this.tokenGenerator.generate(account.id)
+        const accessToken = await this.encrypter.encrypt(account.id)
         await this.updateAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
