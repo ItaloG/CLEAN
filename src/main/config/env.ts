5cb5e5db2dad957331c2727bdefaa044b3326892
Italo Gabriel <italog03@gmail.com>
2022-06-06 10:54:03 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://mongo:mongo@clean-node-api.kr3jksf.mongodb.net/?retryWrites=true&w=majority',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
