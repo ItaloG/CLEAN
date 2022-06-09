9b2dae758f6cd1afc7fb56a48e8b9fe5e4887211
Italo Gabriel <italog03@gmail.com>
2022-06-09 09:41:05 -0300

    refactor: move validation to its own layer

--- src/main/factories/conttrollers/signup/signup-validation-factory.spec.ts
+++ src/main/factories/conttrollers/signup/signup-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { RequiredFieldValidation, CompareFieldValidation, EmailValidation, ValidationComposite } from '../../../../presentation/helpers/validators'
-import { EmailValidator } from '../../../../presentation/protocols/emailValidator'
+import { RequiredFieldValidation, CompareFieldValidation, EmailValidation, ValidationComposite } from '../../../../validation/validators'
+import { EmailValidator } from '../../../../validation/protocols/emailValidator'
 import { Validation } from '../../../../presentation/protocols/validation'
 import { makeSignUpValidation } from './signup-validation-factory'
 
-jest.mock('../../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/presentation/controller/login/login-controller-protocols.ts
+++ src/presentation/controller/login/login-controller-protocols.ts
@@ -1,4 +1,2 @@
 export * from '../../protocols'
-export * from '../../protocols/emailValidator'
-export * from '../../protocols/validation'
 export * from '../../../domain/usecases/authentication'
