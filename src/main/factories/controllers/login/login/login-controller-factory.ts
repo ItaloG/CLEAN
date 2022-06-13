9c4918e0dcb898e0546751948c1e79073a0df98e
Italo Gabriel <italog03@gmail.com>
2022-06-13 09:14:33 -0300

    refactor: organize mais layer folders

---
+++ src/main/factories/controllers/login/login/login-controller-factory.ts
@@ -0,0 +1,10 @@
+import { LoginController } from '../../../../../presentation/controller/login/login/login-controller'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeLoginValidation } from './login-validation-factory'
+
+export const makeLoginController = (): Controller => {
+  const controller = new LoginController(makeDbAuthentication(), makeLoginValidation())
+  return makeLogControllerDecorator(controller)
+}
---
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -0,0 +1,10 @@
+import { AddSurveyController } from '../../../../../presentation/controller/survey/add-survey/add-survey-controller'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeDbAddSurvey } from '../../../usecases/survey/add-survey/db-add-survey-factory'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+export const makeAddSurveyController = (): Controller => {
+  const controller = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
+  return makeLogControllerDecorator(controller)
+}
