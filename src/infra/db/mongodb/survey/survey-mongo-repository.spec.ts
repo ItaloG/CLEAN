5d6000954c5a569602508b1f42b3817cd75f8d02
Italo Gabriel <italog03@gmail.com>
2022-06-17 14:35:44 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -16,6 +16,22 @@ const makeFakeSurvey = (): AddSurveyModel => ({
   date: new Date()
 })
 
+const makeFakeSurveys = (): AddSurveyModel[] => ([{
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+}, {
+  question: 'other_question',
+  answers: [{
+    image: 'other_image',
+    answer: 'other_answer'
+  }],
+  date: new Date()
+}])
+
 const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository()
 }
@@ -34,10 +50,23 @@ describe('Name of the group', () => {
     await MongoHelper.disconnect()
   })
 
-  test('should create an survey on success', async () => {
-    const sut = makeSut()
-    await sut.add(makeFakeSurvey())
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('add()', () => {
+    test('should create an survey on success', async () => {
+      const sut = makeSut()
+      await sut.add(makeFakeSurvey())
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('should load all surveys on success', async () => {
+      await surveyCollection.insertMany(makeFakeSurveys())
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
   })
 })
