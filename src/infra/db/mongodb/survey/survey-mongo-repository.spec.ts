e94044965c15ccdc68f8d1e40f27d017948e07d2
Italo Gabriel <italog03@gmail.com>
2022-06-17 14:37:45 -0300

    test: ensure SurveyMongoRepository load empty

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -68,5 +68,11 @@ describe('Name of the group', () => {
       expect(surveys[0].question).toBe('any_question')
       expect(surveys[1].question).toBe('other_question')
     })
+
+    test('should load empty list', async () => {
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(0)
+    })
   })
 })
