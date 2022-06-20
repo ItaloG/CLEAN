aac4f67061c135f0653393fc89be218ea90399d0
Italo Gabriel <italog03@gmail.com>
2022-06-20 11:57:47 -0300

    feat: ensure SurveyMongoRepository load survey by id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,11 @@
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
+import { LoadSurveysByIdRepository } from '@/data/usecases/load-survey-by-id/db-load-surveys-protocols'
 import { LoadSurveysRepository, SurveyModel } from '@/data/usecases/load-surveys/db-load-surveys-protocols'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { MongoHelper } from '@/infra/db/mongodb/helper/mongo-helper'
+import { ObjectID } from 'bson'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveysByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
@@ -14,5 +16,11 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     const surveys = await surveyCollection.find().toArray()
     const formattedSurveys = surveys.map(survey => MongoHelper.map(survey))
     return formattedSurveys
+  }
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const survey = await surveyCollection.findOne({ _id: new ObjectID(id) })
+    return MongoHelper.map(survey)
   }
 }
