6075e7f285bd3fc685d8c4099fe9f44a5f82f0d2
Italo Gabriel <italog03@gmail.com>
2022-06-27 15:46:17 -0300

    test: esnure SurveyResultMongoRepository loads curveyResults correctly

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -25,20 +25,18 @@ const makeSurvey = async (): Promise<SurveyModel> => {
     }],
     date: new Date()
   }
-  const res = await surveyCollection.insertOne(survey)
 
+  const res = await surveyCollection.insertOne(survey)
   return Object.assign({}, survey, { id: res.insertedId.toString() })
 }
 
 const makeAccount = async (): Promise<AccountModel> => {
   const account = {
     name: 'any_name',
-    email: 'any_email',
+    email: 'any_email@mail.com',
     password: 'any_password'
   }
-
   const res = await accountCollection.insertOne(account)
-
   return Object.assign({}, account, { id: res.insertedId.toString() })
 }
 
@@ -65,19 +63,18 @@ describe('Survey Mongo Repository', () => {
       const survey = await makeSurvey()
       const account = await makeAccount()
       const sut = makeSut()
-      const surveyResult = await sut.save({
+      await sut.save({
         surveyId: survey.id,
         accountId: account.id,
         answer: survey.answers[0].answer,
         date: new Date()
       })
+      const surveyResult = await surveyResultCollection.findOne({
+        surveyId: new ObjectId(survey.id),
+        accountId: new ObjectId(account.id)
+      })
+
       expect(surveyResult).toBeTruthy()
-      expect(surveyResult.surveyId.toString()).toEqual(survey.id)
-      expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
-      expect(surveyResult.answers[0].count).toBe(1)
-      expect(surveyResult.answers[0].percent).toBe(100)
-      expect(surveyResult.answers[1].count).toBe(0)
-      expect(surveyResult.answers[1].percent).toBe(0)
     })
 
     test('Should update survey result if its not new', async () => {
@@ -90,19 +87,20 @@ describe('Survey Mongo Repository', () => {
         date: new Date()
       })
       const sut = makeSut()
-      const surveyResult = await sut.save({
+      await sut.save({
         surveyId: survey.id,
         accountId: account.id,
         answer: survey.answers[1].answer,
         date: new Date()
       })
+      const surveyResult = await surveyResultCollection
+        .find({
+          surveyId: new ObjectId(survey.id),
+          accountId: new ObjectId(account.id)
+        })
+        .toArray()
       expect(surveyResult).toBeTruthy()
-      expect(surveyResult.surveyId.toString()).toEqual(survey.id)
-      expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
-      expect(surveyResult.answers[0].count).toBe(1)
-      expect(surveyResult.answers[0].percent).toBe(100)
-      expect(surveyResult.answers[1].count).toBe(0)
-      expect(surveyResult.answers[1].percent).toBe(0)
+      expect(surveyResult.length).toBe(1)
     })
   })
 
