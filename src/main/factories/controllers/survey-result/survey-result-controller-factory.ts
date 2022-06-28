8dc681332ce9d208c08f8aa8d877b1e4cf142adc
Italo Gabriel <italog03@gmail.com>
2022-06-28 09:10:00 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/main/factories/controllers/survey-result/survey-result-controller-factory.ts
+++ src/main/factories/controllers/survey-result/survey-result-controller-factory.ts
@@ -1,7 +1,7 @@
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { SaveSurveyResultController } from '@/presentation/controller/survey-result/save-survey-result/save-survey-result-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/db-save-survey-result-factory'
+import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory'
 import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
 
 export const makeSaveSurveyResultController = (): Controller => {
