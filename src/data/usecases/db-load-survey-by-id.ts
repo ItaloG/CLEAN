ec66a8ef048ea52bc0ecebf393309ce60531868a
Italo Gabriel <italog03@gmail.com>
2022-07-04 11:17:50 -0300

    refactor: add types to LoadSurveyById usecase

--- src/data/usecases/db-load-survey-by-id.ts
+++ src/data/usecases/db-load-survey-by-id.ts
@@ -1,10 +1,9 @@
 import { LoadSurveyById } from '@/domain/usecases'
-import { SurveyModel } from '@/domain/models'
 import { LoadSurveyByIdRepository } from '@/data/protocols'
 
 export class DbLoadSurveyById implements LoadSurveyById {
   constructor (private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
-  async loadById (id: string): Promise<SurveyModel> {
+  async loadById (id: string): Promise<LoadSurveyByIdRepository.Result> {
     const survey = await this.loadSurveyByIdRepository.loadById(id)
     return survey
   }
